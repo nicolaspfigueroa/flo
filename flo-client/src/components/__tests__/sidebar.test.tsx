793620f5772ac13f315349b10643efd2ed0be09b
nicolaspfigueroa <nicolaspfigueroa@gmail.com>
2022-06-21 12:08:48 -0600

    added journey details

--- flo-client/src/components/__tests__/sidebar.test.tsx
+++ flo-client/src/components/__tests__/sidebar.test.tsx
@@ -1,4 +1,6 @@
-import { SideBar } from '../Sidebar';
+import  SideBar  from '../Sidebar';
+import * as ReactDOM from 'react-dom';
+const journeyDetails = require('./journey-details-db.json');
 
 describe('Sidebar component tests', () => {
   let container: HTMLDivElement
@@ -6,6 +8,7 @@ describe('Sidebar component tests', () => {
   beforeEach(() => {
     container = document.createElement('div');
     document.body.appendChild(container);
+    ReactDOM.render(<SideBar journeys={journeyDetails}/>, container);
   })
 
 })
