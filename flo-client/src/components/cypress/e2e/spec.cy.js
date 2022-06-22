7d921c4e3e96862624bc831252ae3190343967a1
nicolaspfigueroa <nicolaspfigueroa@gmail.com>
2022-06-22 17:29:41 -0600

    cypress implemented

--- flo-client/src/components/cypress/e2e/spec.cy.js
+++ flo-client/src/components/cypress/e2e/spec.cy.js
@@ -6,7 +6,18 @@ describe('navigation', () => {
       name: /new york 4 days/i
     }).click();
     //checks / unchecks boxes
+    cy.contains('Empire State Buildin')
+    .parent()
+    .find('input[type=checkbox]')
+    .check()
     //adds restaurant
+    cy.get('input[name="name"]').type('Restaurant 1').should('have.value', 'Restaurant 1');
+    cy.get('input[name="address"]').type('123 Main St.').should('have.value', '123 Main St.');
+    cy.get('input[name="cuisineTypes"]').type('Burgers').should('have.value', 'Burgers');
+    cy.get('input[name="suggestedFor"]').type('Me').should('have.value', 'Me');
+    cy.get('#create').click();
     //adds note
+    cy.get('input[name="note"]').type('Adding note').should('have.value', 'Adding note');
+    cy.get('#notebutton').click();
   })
 })
